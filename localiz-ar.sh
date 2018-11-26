8430fb63c298b7e88680334ad6455fd4c199c787
deztino <32746658+deztino@users.noreply.github.com>
2018-11-26 14:57:57 -0300

    Update localiz-ar.sh

--- localiz-ar.sh
+++ localiz-ar.sh
@@ -1,7 +1,7 @@
 echo =============================================
 echo Taller de Odoo 002
 echo Instalando localizacion Argentina
-echo taller-de-odoo-.slack.com Modified by Deztino v0.3
+echo taller-de-odoo-.slack.com Modified by Deztino v0.4
 echo =============================================
 OE_USER="ubuntu"
 OE_HOME="/$OE_USER"
@@ -30,6 +30,8 @@ pip3 install --user bokeh
 pip3 install --user PyYAML
 pip3 install --user bkcharts
 pip3 install --user tornado
+pip3 install --user zpl2
+sudo apt-get install libcups2-dev
 pip3 install --user pycups
 
 echo =============================================
@@ -62,9 +64,11 @@ echo La lista de --addons-path= la generamos con el comando siguiente;
 echo find /home$OE_HOME/odoo/11/sources/ -mindepth 1 -maxdepth 1 -type d | sort | tr '\n' ','
 echo =============================================
 # Lista de es_BO, es_CL, es_PE, es_PY, es_UY, es_MX, es_VE
+cd /home$OE_HOME/odoo/11/
 odoo/odoo-bin -c odoo-localizacionargentina.conf -s --stop-after-init --db_host=localhost --db_user=$OE_USER --db_password=$OE_USER --addons-path=/home$OE_HOME/odoo/11/odoo/addons,/home/odoo/odoo/11/sources/account-closing,/home/odoo/odoo/11/sources/account-financial-tools,/home/odoo/odoo/11/sources/account-invoicing,/home/odoo/odoo/11/sources/account-payment,/home/odoo/odoo/11/sources/bank-payment,/home/odoo/odoo/11/sources/currency,/home/odoo/odoo/11/sources/hr-timesheet,/home/odoo/odoo/11/sources/ingadhoc-account-analytic,/home/odoo/odoo/11/sources/ingadhoc-account-financial-tools,/home/odoo/odoo/11/sources/ingadhoc-account-invoicing,/home/odoo/odoo/11/sources/ingadhoc-account-payment,/home/odoo/odoo/11/sources/ingadhoc-aeroo_reports,/home/odoo/odoo/11/sources/ingadhoc-argentina-reporting,/home/odoo/odoo/11/sources/ingadhoc-argentina-sale,/home/odoo/odoo/11/sources/ingadhoc-hr,/home/odoo/odoo/11/sources/ingadhoc-miscellaneous,/home/odoo/odoo/11/sources/ingadhoc-multi-company,/home/odoo/odoo/11/sources/ingadhoc-odoo-argentina,/home/odoo/odoo/11/sources/ingadhoc-partner,/home/odoo/odoo/11/sources/ingadhoc-patches,/home/odoo/odoo/11/sources/ingadhoc-payment,/home/odoo/odoo/11/sources/ingadhoc-product,/home/odoo/odoo/11/sources/ingadhoc-project,/home/odoo/odoo/11/sources/ingadhoc-purchase,/home/odoo/odoo/11/sources/ingadhoc-reporting-engine,/home/odoo/odoo/11/sources/ingadhoc-sale,/home/odoo/odoo/11/sources/ingadhoc-stock,/home/odoo/odoo/11/sources/ingadhoc-website,/home/odoo/odoo/11/sources/oca-purchase-workflow,/home/odoo/odoo/11/sources/oca-sale_workflow,/home/odoo/odoo/11/sources/oca-web,/home/odoo/odoo/11/sources/partner-contact,/home/odoo/odoo/11/sources/product-attribute,/home/odoo/odoo/11/sources/purchase-workflow,/home/odoo/odoo/11/sources/queue,/home/odoo/odoo/11/sources/reporting-engine,/home/odoo/odoo/11/sources/sale-workflow,/home/odoo/odoo/11/sources/server-auth,/home/odoo/odoo/11/sources/server-tools,/home/odoo/odoo/11/sources/server-ux,/home/odoo/odoo/11/sources/social,/home/odoo/odoo/11/sources/stock-logistics-workflow,/home/odoo/odoo/11/sources/web -d TALLERODOO_DB_11 --logfile=/home/odoo/odoo/11/odoo-tallerodoo.log --http-port=8069 --longpolling-port=8072 --http-interface=0.0.0.0 --data-dir=/home/odoo/odoo/11/datadir --load-language=es_AR
 
 echo =============================================
 echo Levantamos Odoo v11 Localizacion Argentina
 echo =============================================
+cd /home$OE_HOME/odoo/11/
 odoo/odoo-bin -c odoo-localizacionargentina.conf &
