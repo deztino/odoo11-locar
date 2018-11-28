0e1a35c962b9e3681331fac87efd99dc91965d74
deztino <32746658+deztino@users.noreply.github.com>
2018-11-28 15:41:42 -0300

    Create adhoc-deps-from-yenthe.sh

---
+++ adhoc-deps-from-yenthe.sh
@@ -0,0 +1,32 @@
+echo Instalando dependencias para una instalacion desde Script Yenthe666
+echo
+pip3 install pycups
+pip3 install --user Genshi
+echo comment sudo nano genshi
+pip3 install --user git+https://github.com/adhoc-dev/aeroolib@master-fix-ods
+pip3 install --user git+https://github.com/aeroo/currency2text.git
+pip3 install --user numpy
+echo comment sudo nano sources/ingadhoc-website/requirements.txt numpy
+pip3 install --user fdfgen
+echo cooment sudo nano sources/reporting-engine/requirements.txt
+echo instalando paq1...
+pip3 install --user zpl2 checksumdir raven pysftp acme_tiny IPy
+pip3 install --user pyopenssl
+pip3 install --user httplib2==0.9.2
+pip3 install --user pysimplesoap
+pip3 install --user git+https://github.com/urbina05/pyafipws@patch-2
+echo comment sudo nano
+pip3 install --user pygments
+pip3 install --user dnspython
+pip3 install --user email_validator
+pip3 install --user schema
+pip3 install --user lasso
+pip3 install --user pyotp
+pip3 install --user tornado
+pip3 install --user bkcharts==0.2
+pip3 install --user bokeh==0.12.7
+pip3 install --user git+https://github.com/OCA/openupgradelib/@master
+echo comment suda nano
+
+
+
