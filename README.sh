68bb486206e6d6aae387ed8228cf3ff1ce378517
deztino <32746658+deztino@users.noreply.github.com>
2018-12-01 02:31:02 -0300

    Update README.sh

--- README.sh
+++ README.sh
@@ -1,5 +1,10 @@
+echo
 echo Arrancador deZtino Odoo Argentina Instalador VERSION 0.5
 sudo git clone https://github.com/deztino/odoo11-locar.git
 cd odoo11-locar
-sudo chmod +x *
+/bin/sleep 3
+sudo chmod +x * 
+/bin/sleep 3
+echo ls -ltr
+/bin/sleep 3
 sudo -H ./script.sh
