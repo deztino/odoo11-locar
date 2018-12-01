992043a1708988659a97fde292ae5b1bb017a525
deztino <32746658+deztino@users.noreply.github.com>
2018-12-01 01:38:47 -0300

    Update virtualenv-instalar-odoo.sh

--- virtualenv-instalar-odoo.sh
+++ virtualenv-instalar-odoo.sh
@@ -1,8 +1,10 @@
 echo =============================================
-echo Taller de Odoo 001
-echo Instalando localizacion Latinoamericana
-echo taller-de-odoo-.slack.com modified by deztino v1.1
+echo Instalacion de Odoo con VirtualEnv by deztino v1.2
 echo =============================================
+if [ "$(id -u)" = "0" ]; then
+    echo "Lo siento, debe ejecutar el script sin SUDO"
+    exit 1
+fi
 OE_USER="ubuntu"
 OE_HOME="/$OE_USER"
 OE_DBNAME="sanignaciosrl"
@@ -43,6 +45,7 @@ pip3 install --user --upgrade setuptools
 sudo -H pip3 install --user PyPDF2
 sudo -H pip3 install --user werkzeug
 sudo -H pip3 install --user python-dateutil
+sudo -H pip3 install --user pytz
 echo -Listo!
 echo =============================================
 echo Clonando e instalando ODOO 
@@ -59,15 +62,16 @@ echo -Listo!
 echo =============================================
 echo Generando odoo.conf y Configurando
 echo =============================================
+echo Instalando requerimientos desde odoo/requirements.txt ...
 pip3 install -r odoo/requirements.txt
 pip3 install --user phonenumbers
 pip3 install --user passlib
-pip3 install --user pytz
 pip3 install --user babel
-
+echo -Listo!
 # Lista de es_BO, es_CL, es_PE, es_PY, es_UY, es_MX, es_VE
+echo Ejecutando creacion del archivo de configuracion de Odoo11:
 odoo/odoo-bin -c odoo.conf -s --stop-after-init --db_host=localhost --db_user=odoo --db_password=odoo --addons-path=/home$OE_HOME/odoo/11/odoo/addons -d $OE_DBNAME --logfile=/home$OE_HOME/odoo/11/odoo-$OE_DBNAME.log --http-port=8069 --longpolling-port=8072 --http-interface=0.0.0.0 --data-dir=/home$OE_HOME/odoo/11/datadir --load-language=es_AR
-
+echo -Listo
 echo =============================================
 echo Levantamos Odoo v11 Localizacion Latinoamericana
 echo =============================================
